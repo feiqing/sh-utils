d2e6d8da0aec58e8cd0a08777343a1064b0441f9
feiqing <zhujifang666@163.com>
2016-11-07 15:54:00 +0800

    update ssh

--- ssh-gateway.sh
+++ ssh-gateway.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect
 
-## 将[_xxx_] 替换成你自己实际的账户信息
+## 将[_xxx_] 替换成你自己的实际信息
 
-set addr [_your_name_]@relay00.idcvdian.com
+set addr [_your_name_]@[_server_addr_]
 set password [_your_password_]
-set decode_key [ exec sh -c {curl -s http://10.1.100.231:8080/decode.do?secret_key=[_your_secret_code_on_http://wdren.vdian.net/user/initDynamicKey]} ]
+set decode_key [ exec sh -c {curl -s http://[_decode_server_ip_]:8080/decode.do?secret_key=[_your_secret_code_]} ]
 
 set timeout 30
 spawn ssh $addr
